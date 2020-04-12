da5b70a48784ac01bae5c21471a2df1fe92e63fa
Gonzalo Gras Cantou <guusygonzalo@gmail.com>
2020-04-12 20:19:41 -0300

    Add LessEquals operator

---
+++ src/operators/relationals/lessEquals/LessEquals.spec.ts
@@ -0,0 +1,26 @@
+import LessEquals from "./LessEquals";
+
+describe('LessEquals operator', () => {
+    const accessor = (object: any) => object
+
+    describe('When the value is less than expected', () => {
+        const operation = new LessEquals({ expectedValue: 20, accessor }).initialize(10)
+        it('return true', () => {
+            expect(operation.evaluate()).toBe(true);
+        });
+    });
+
+    describe('When the value is greater than the expected ', () => {
+        const operation = new LessEquals({ expectedValue: 2, accessor }).initialize(10)
+        it('return false', () => {
+            expect(operation.evaluate()).toBe(false);
+        });
+    });
+
+    describe('when the values are equals', () => {
+        const operation = new LessEquals({ expectedValue: 40, accessor }).initialize(40)
+        it('return true', () => {
+            expect(operation.evaluate()).toBe(true);
+        });
+    });
+});
